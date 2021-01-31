4c7b82b475ec0b340628cbda50904a4159237c1a
Brandon Starcheus <bstarcheus@gmail.com>
2021-01-31 14:13:44 -0500

    Add debug flag

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -6,13 +6,24 @@
 SymbolTable symb;
 
 int main(int argc, char* argv[]) {
+    bool dbg = false;
+    std::string filename;
+
     if (argc < 2) {
-        std::cout << "Error: Missing positional argument filename" << std::endl;
+        std::cout << "Error: Missing positional argument filename"\
+        << std::endl;
+        return 1;
+    } else if (argc <= 3) {
+        for (int i = 2; i < argc; i++) {
+            if (!strcmp(argv[i], "-d") || 
+                !strcmp(argv[i], "--debug")) {
+                dbg = true;
+            }
+        }
+    } else {
+        std::cout << "Error: Too many arguments" << std::endl;
         return 1;
     }
-
-    // TODO add debug flag argument
-    bool dbg = true;
 
     Scanner s(dbg);
     if (!s.openFile(argv[1])) {
@@ -22,12 +33,16 @@ int main(int argc, char* argv[]) {
 
     Parser p(&s, dbg);
     bool success = p.parse();
-    std::cout << "Parse success: " << success << std::endl;
 
-    std::cout << std::endl << std::endl << "Symbol Table:" << std::endl;
-    // Check symbol table entries
-    for (SymTabMap::iterator it = symb.begin(); it != symb.end(); ++it) {
-        std::cout << getTokenTypeName(it->second) << " " << it->second.val << std::endl;
+    if (dbg) {
+        std::cout << "Parse success: " << success << std::endl;
+
+        std::cout << std::endl << std::endl << "Symbol Table:" << std::endl;
+        // Check symbol table entries
+        for (SymTabMap::iterator it = symb.begin(); it != symb.end(); ++it) {
+            std::cout << getTokenTypeName(it->second) << " "\
+            << it->second.val << std::endl;
+        }
     }
 
     return 0;
